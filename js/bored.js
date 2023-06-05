1e13ce5008e203a9c247059585ac94d365eab6ae
qazazx20 <qazazx20@daum.net>
2023-06-05 19:59:18 +0900

    fix: api link

--- js/bored.js
+++ js/bored.js
@@ -1,6 +1,6 @@
 const bored = document.querySelector("#bored span:nth-of-type(2)");
 
-const url = `http://www.boredapi.com/api/activity/`;
+const url = `http://www.boredapi.com/api/activity`;
 
 fetch(url)
     .then((response) => response.json())
